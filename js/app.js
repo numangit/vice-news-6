211008f2b480c3aae234c8b7c7d685cd5f7a5d8b
numangit <abdulnumanjob@gmail.com>
2022-09-03 03:41:11 +0600

    event Hnadler added for the catagory buttons

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-// const loadData = async () => {
-//     const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
-//     const data = res.json();
+// const loadDataCatagory = async (searchText) => {
+//     const res = await fetch(`https://openapi.programming-hero.com/api/news/categories`);
+//     const data = await res.json();
+//     displayCatagory(data.data.news_category)
 // }
 
 //=============== catagory section codes =====================
@@ -15,16 +16,49 @@ const loadDataCatagory = () => {
 loadDataCatagory();
 //function to display catagories
 const displayCatagory = (catagories) => {
-    console.log(catagories)
+    // console.log(catagories)
     const catagoryContainer = document.getElementById('catagory-container');
     catagories.forEach(catagory => {
         const newLi = document.createElement("li");
         newLi.classList.add("nav-item");
         newLi.innerHTML = `
         <li class="nav-item">
-            <a class="nav-link text-dark" aria-current="page" href="#" onclick="">${catagory.category_name}</a>
+            <a class="nav-link" aria-current="page" href="#" onclick="loadNews('${catagory.category_id}')">${catagory.category_name}</a>
         </li>
         `
         catagoryContainer.appendChild(newLi)
+    });
+}
+
+//================= news card section codes ==============================
+
+// function to load the news based on catagory
+const loadNews = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+}
+// function to display the news
+const displayNews = (newsList) => {
+    const newsCardContainer = document.getElementById('news-card-container');
+    newsCardContainer.innerHTML = '';
+    newsList.forEach(news => {
+        console.log(news);
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('col');
+        newDiv.innerHTML = `
+        <div class="card flex-sm-row">
+        <img src="${news.thumbnail_url}">
+        <div class="card-body text-start align-items-center pt-lg-5">
+            <h5 class="card-title text-secondary fw-bold">Fundamental Of UI/UX Design
+            </h5>
+            <p class="card-text text-muted fw-light">
+                We will be covering here about the Fundamental Of UI/UX Design.
+            </p>
+            <h6 class="text-info fw-bold">Price : $25</h6>
+        </div>
+    </div>
+        `;
+        newsCardContainer.appendChild(newDiv);
     });
 }
