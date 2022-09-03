7b67c4c2c70b3fc21fdc5957530183b516849d0e
numangit <abdulnumanjob@gmail.com>
2022-09-03 06:49:38 +0600

    news cards are added

--- js/app.js
+++ js/app.js
@@ -23,11 +23,22 @@ const displayCatagory = (catagories) => {
         newLi.classList.add("nav-item");
         newLi.innerHTML = `
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#" onclick="loadNews('${catagory.category_id}')">${catagory.category_name}</a>
+            <a class="nav-link text-dark-gray fw-semibold" aria-current="page" href="#" onclick="loadNews('${catagory.category_id}')">${catagory.category_name}</a>
         </li>
         `
         catagoryContainer.appendChild(newLi)
     });
+}
+
+//================ result message section =========================
+const showMessage = (resultAmmount = 0) => {
+    const messageSection = document.getElementById('message-container')
+    messageSection.innerText = '';
+    if (resultAmmount === 0) {
+        messageSection.innerText = "No Result Found"
+    } else {
+        messageSection.innerText = `${resultAmmount} Results Found`
+    }
 }
 
 //================= news card section codes ==============================
@@ -37,6 +48,7 @@ const loadNews = (id) => {
     fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
         .then(res => res.json())
         .then(data => displayNews(data.data))
+        .catch(showMessage())//problemmmmmmmm it show no result everytime
 }
 // function to display the news
 const displayNews = (newsList) => {
@@ -47,18 +59,30 @@ const displayNews = (newsList) => {
         const newDiv = document.createElement('div');
         newDiv.classList.add('col');
         newDiv.innerHTML = `
-        <div class="card flex-sm-row">
-        <img src="${news.thumbnail_url}">
-        <div class="card-body text-start align-items-center pt-lg-5">
-            <h5 class="card-title text-secondary fw-bold">Fundamental Of UI/UX Design
-            </h5>
-            <p class="card-text text-muted fw-light">
-                We will be covering here about the Fundamental Of UI/UX Design.
-            </p>
-            <h6 class="text-info fw-bold">Price : $25</h6>
-        </div>
-    </div>
+        <div class="card flex-sm-row p-2" onclick="loadPhoneDetails('${phone.slug}')" data-bs-toggle="modal" data-bs-target="#newsModal">
+            <img src="${news.thumbnail_url}">
+            <div class="card-body text-start align-items-center pt-lg-3">
+                    <h5 class="card-title text-secondary fw-bold fs-3">${news.title}</h5>
+                    <p class="card-text text-muted fw-light pt-lg-3">${news.details.length < 200 ? news.details :
+                news.details.slice(0, 300) + "..."}</p>
+                <div class="d-lg-flex mx-auto mt-5">
+                    <div class="d-flex w-25 justify-space-around mx-auto">
+                        <div class=" w-25 " >
+                            <img class="img-fluid rounded-circle" src="${news.author.img ? news.author.img : " No Result Found"}" alt="">
+                        </div>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <p class="fw-semibold ps-3">${news.author.name ? news.author.name : "No Result Found"}</p>                  
+                        </div>
+                    </div>
+                    <div class="w-25 text-center d-flex justify-content-center align-items-center mx-auto"><i class="fa-regular fa-eye"></i>&nbsp;&nbsp;<strong>${news.total_view}</strong></div>
+                    <div class="w-25 text-center d-flex justify-content-center align-items-center mx-auto "> <button class="btn"><i class="fa-solid fa-arrow-right fs-3"></i></button></div>
+                </div>
+            </div>
+         </div>
+
         `;
         newsCardContainer.appendChild(newDiv);
+        showMessage(newsList.length);
+        // console.log(newsAmmount)
     });
 }
